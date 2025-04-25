a572679e2fd18fd3a4d63276b15066f6248c6fc5
alla0810 <kyosook.shin@gmail.com>
2025-04-25 13:19:12 -0500

    visceral fat

--- xiaomi/ble_scale_reader.py
+++ xiaomi/ble_scale_reader.py
@@ -115,23 +115,25 @@ def get_visceral_fat(weight):
     sex = SEX_OF_THIS_PERSON
 
     if sex == "Female":
-        threshold = (height * 0.5) - 13
-        if weight > threshold:
+        if weight > (13 - (height * 0.5)) * -1:
             subsubcalc = ((height * 1.45) + (height * 0.1158) * height) - 120
             subcalc = weight * 500 / subsubcalc
             vfal = (subcalc - 6) + (age * 0.07)
         else:
-            subcalc = 0.691 + (height * -0.0024) * 2
-            vfal = ((height * 0.027) - (subcalc * weight)) * -1 + (age * 0.07) - age
-    elif height < weight * 1.6:
-        subcalc = (height ** 2 * 0.0826) - (height * 0.4)
-        vfal = (weight * 305) / (subcalc + 48) - 2.9 + (age * 0.15)
+            subcalc = 0.691 + (height * -0.0024) + (height * -0.0024)
+            vfal = (((height * 0.027) - (subcalc * weight)) * -1) + (age * 0.07) - age
+
     else:
-        subcalc = 0.765 - height * 0.0015
-        vfal = (weight * subcalc - height * 0.143) + (age * 0.15) - 5.0
+        if height < weight * 1.6:
+            subcalc = ((height * 0.4) - (height * (height * 0.0826))) * -1
+            vfal = ((weight * 305) / (subcalc + 48)) - 2.9 + (age * 0.15)
+        else:
+            subcalc = 0.765 + height * -0.0015
+            vfal = (((height * 0.143) - (weight * subcalc)) * -1) + (age * 0.15) - 5.0
 
     return check_value_overflow(vfal, 1, 50)
         
+
 
 
 def calculate_body_metrics(weight, impedance):
